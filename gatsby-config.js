1bde0a04c939f361076a0dc1cb6b443856273441
Sean McDonnell <smcdonnell1@gmail.com>
2019-07-05 16:29:49 -0400

    updated site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Miami Beach Community Church',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A church for the open door, open mind, and open heart.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
